a8b65a2c6d506255e6668a5159fcff2aa0973aad
CH19 <chcc190505@gmail.com>
2024-02-18 22:24:22 -0400

    aded all things for numbers descomposition

--- descomponer.py
+++ descomponer.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 def es_primo(num):
     for n in range(2, num):
         if num % n == 0:
@@ -9,16 +10,23 @@ def descomponer(num: int):
     actual_num = num
     descomposition = []
     primos = []
-    for i in range(2, num):
+    for i in range(2, (num + 1)):
         if(es_primo(i)):
             primos.append(i)
+    try:
+       if( primos.index(num)):
+           print("Es un numero primo")
+           return
+    except ValueError:
+        pass
     while actual_num > 1:
         for i in range(len(primos)):
             if(actual_num % primos[i] == 0): 
                 actual_num /= primos[i]
-                descomposition.append({"division": int(actual_num), "divisor": primos[i]})   
-                break 
-        print(actual_num)
+                descomposition.append([int(actual_num),primos[i]])   
+                break
+    print("Numero", num)
+    print(tabulate(descomposition, headers=["Division", "Divisor"], tablefmt="grid"))
+num = input("Ingresa el numero que deseas descomponer: ")
 
-    print(descomposition)
-descomponer(16)
+descomponer(int(num))
